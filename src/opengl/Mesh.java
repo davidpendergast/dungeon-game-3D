fc6d9d978e094fb9f879167d330d132b15bee543
dpendergast <dlp75@case.edu>
2017-03-20 18:11:22 -0400

    got vertex color attributes working

--- src/opengl/Mesh.java
+++ src/opengl/Mesh.java
@@ -14,15 +14,20 @@ public class Mesh {
     private final int vaoId;
     private final int posVboId;
     private final int idxVboId;
+    private final int colorVboId;
     private final int vertexCount;
     
-    public Mesh(float[] positions, int[] indices) {
+    public Mesh(float[] positions, float[] colors, int[] indices) {
         FloatBuffer verticesBuffer = null;
+        FloatBuffer colorBuffer = null;
         IntBuffer indicesBuffer = null;
         try {
             vertexCount = indices.length;
             verticesBuffer = MemoryUtil.memAllocFloat(positions.length);
             verticesBuffer.put(positions).flip();
+            
+            colorBuffer = MemoryUtil.memAllocFloat(colors.length);
+            colorBuffer.put(colors).flip();
             
             indicesBuffer = MemoryUtil.memAllocInt(indices.length);
             indicesBuffer.put(indices).flip();
@@ -36,14 +41,23 @@ public class Mesh {
             glVertexAttribPointer(0,3, GL_FLOAT, false, 0, 0);
             glBindBuffer(GL_ARRAY_BUFFER, 0);
             
+            colorVboId = glGenBuffers();
+            glBindBuffer(GL_ARRAY_BUFFER, colorVboId);
+            glBufferData(GL_ARRAY_BUFFER, colorBuffer, GL_STATIC_DRAW);
+            glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
+            
             idxVboId = glGenBuffers();
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxVboId);
             glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
+            
             
             glBindVertexArray(0);
         } finally {
             if (verticesBuffer != null) {
                 MemoryUtil.memFree(verticesBuffer);
+            }
+            if (colorBuffer != null) {
+                MemoryUtil.memFree(colorBuffer);
             }
             if (indicesBuffer != null) {
                 MemoryUtil.memFree(indicesBuffer);
--- src/opengl/Renderer.java
+++ src/opengl/Renderer.java
@@ -33,8 +33,8 @@ public class Renderer {
         
         // bind the VAO
         glBindVertexArray(mesh.getVaoId());
-        glEnableVertexAttribArray(0);
-        
+        glEnableVertexAttribArray(0);   // enable index lookups
+        glEnableVertexAttribArray(1);   // enable color
         // draw the vertices
         glDrawElements(GL_TRIANGLES, mesh.getVertexCount(), GL_UNSIGNED_INT, 0);
         
